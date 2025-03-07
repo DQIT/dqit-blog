df1ee7d1d8844479b6234239245f90f7c340a2a5
HuangQing <mr_hqing@163.com>
2025-03-08 05:02:03 +0800

    init

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,25 @@
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    bundler: viteBundler(),
+    theme: defaultTheme({
+        logo: '/image/logo.png',
+        repo: 'DQIT/dqit-blog',
+        navbar: [
+            {
+                text: 'MyGit',
+                link: 'https://git.dqit.top',
+            }
+        ]
+    }),
+
+    head: [
+        ['link',{ rel: 'icon', href: '/favicon.ico' }]
+    ],
+
+    lang: 'zh-CN',
+    title: '一笔清泉字',
+    description: ' ',
+})
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: /image/logo.png
+heroText: 一笔清泉字
+footerHtml: ture
+footer: <a href="https://beian.miit.gov.cn/" target="_blank">皖ICP备2023020687号-1</a> | copyright © 2023-present 一笔清泉字
+---
